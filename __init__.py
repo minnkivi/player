5fd7c443ea1ba6b556c35f582600fef0faacb1cd
minna <minnkivi@yahoo.com>
2021-05-05 07:43:43 +0000

    oikeat tiedostot

--- __init__.py
+++ __init__.py
@@ -1,15 +1,67 @@
+import os
 from mycroft import MycroftSkill, intent_file_handler
 
+def is_playing():
+	playing = False
+	check = os.popen('ps ax | grep cmus | grep -v "grep"').readlines()
+	if len(check) > 0:
+		playing = True
+	return playing
 
-class Testplay(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+class TestPlay(MycroftSkill):
 
-    @intent_file_handler('testplay.intent')
-    def handle_testplay(self, message):
-        self.speak_dialog('testplay')
+	def __init__(self):
+		MycroftSkill.__init__(self)
+		self.open_player()
 
+
+	@intent_file_handler('start.intent')
+	def handlle_start_intent(self, message):
+		self.start_player()
+		self.speak_dialog("start")
+
+	@intent_file_handler('pause.intent')
+	def handle_pause_intent(self, message):
+		self.pause_player()
+
+	@intent_file_handler('continue.intent')
+	def handlle_continue_intent(self, message):
+		self.toggle_pause()
+
+	@intent_file_handler('stop.intent')
+	def handle_stop_intent(self, message):
+		self.stop_player()
+
+	@intent_file_handler('next.intent')
+	def handle_next_intent(self, message):
+		self.play_next()
+
+	def open_player(self):
+		if not is_playing():
+			self.start_player()
+
+	def start_player(self):
+		os.system("screen -d -m -S cmus cmus &")
+		os.system('cmus-remote -C "view 2"')
+		#os.system('cmus-remote -C clear')
+		#os.system('cmus-remote -C add -l ~/music/*')
+		os.system('cmus-remote -p')
+
+	def pause_player(self):
+		self.speak_dialog("pause")
+		os.system('cmus-remote -U')
+
+	def toggle_pause(self):
+		self.speak_dialog("continue")
+		os.system('cmus-remote -u')
+
+	def stop_player(self):
+		self.speak_dialog("stop")
+		os.system('cmus-remote -s')
+
+	def play_next(self):
+		self.speak_dialog("next")
+		os.system('cmus-remote -n')
 
 def create_skill():
-    return Testplay()
-
+	return TestPlay()
